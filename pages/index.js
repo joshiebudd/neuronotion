9574781250c77e969e7396a01e8d46c5cb1fa278
joshiebudd <140086428+joshiebudd@users.noreply.github.com>
2023-12-08 20:39:30 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -55,7 +55,8 @@ const IndexPage = () => {
     }
     gtag('js', new Date());
     gtag('config', 'G-FTX0TWXP1E');
-
+    
+const speedInsights = new SpeedInsights(/* options */);
   }, []); // Empty dependency array to run the effect only once
 
   return (
