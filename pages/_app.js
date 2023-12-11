57ff1ec7d22b557847999178ff4fe037fdf9a2a2
joshiebudd <140086428+joshiebudd@users.noreply.github.com>
2023-12-11 10:43:31 +0000

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -46,6 +46,16 @@ function MyApp({ Component, pageProps }) {
         src="/_vercel/insights/script.js"
       />
 
+      {/* Facebook Pixel Script */}
+      <Script
+        strategy="afterInteractive"
+        src="https://connect.facebook.net/en_US/fbevents.js"
+        onLoad={() => {
+          fbq('init', '230622039592089');
+          fbq('track', 'PageView');
+        }}
+      />
+
       <Component {...pageProps} />
 
       <Analytics />
