2098fabe876c4b42a09f1b3cf7b70a79dd2930c0
joshiebudd <140086428+joshiebudd@users.noreply.github.com>
2023-12-11 15:19:51 +0000

    Update BenefitsSection.js

--- components/BenefitsSection.js
+++ components/BenefitsSection.js
@@ -13,53 +13,45 @@ const BenefitsSection = () => {
             The all-in-one system your brain has been crying out for. Everything in one place, with no distractions.
           </p>
         </div>
-        <div className="md:grid md:grid-cols-2 gap-x-16 gap-y-4">
-          <div className="text-center">
-            <div className="mx-auto">
-              <Image
-                src="https://raw.githubusercontent.com/joshiebudd/notionwidgets/main/quickadd_ben.webp"
-                alt="Quick Add Demo"
-                width={320} // Set your desired width here
-                height={320} // Set your desired height here
-              />
-            </div>
+        <div className="md:grid md:grid-cols-2 gap-x-16 gap-y-4 items-center">
+          <div className="flex flex-col items-center">
+            <Image
+              src="https://raw.githubusercontent.com/joshiebudd/notionwidgets/main/quickadd_ben.webp"
+              alt="Quick Add Demo"
+              width={320}
+              height={320}
+            />
             <h3 className="mb-0 text-xl font-bold text-black">Quick Add</h3>
             <p className="mb-4 text-gray-700">Capture notes or ideas fast. Without distractions.</p>
           </div>
-          <div className="text-center">
-            <div className="mx-auto">
-              <Image
-                src="https://raw.githubusercontent.com/joshiebudd/notionwidgets/main/focus_tools_ben.webp"
-                alt="Focus Tools Demo"
-                width={320} // Set your desired width here
-                height={320} // Set your desired height here
-              />
-            </div>
+          <div className="flex flex-col items-center">
+            <Image
+              src="https://raw.githubusercontent.com/joshiebudd/notionwidgets/main/focus_tools_ben.webp"
+              alt="Focus Tools Demo"
+              width={320}
+              height={320}
+            />
             <h3 className="mb-0 text-xl font-bold text-black">ADHD Focus Tools</h3>
             <p className="mb-4 text-gray-700">Custom built-in focus timers, sounds, and tools</p>
           </div>
 
-          <div className="text-center">
-            <div className="mx-auto">
-              <Image
-                src="https://raw.githubusercontent.com/joshiebudd/notionwidgets/main/flashcards_ben.webp"
-                alt="Flashcards Demo"
-                width={320} // Set your desired width here
-                height={320} // Set your desired height here
-              />
-            </div>
+          <div className="flex flex-col items-center">
+            <Image
+              src="https://raw.githubusercontent.com/joshiebudd/notionwidgets/main/flashcards_ben.webp"
+              alt="Flashcards Demo"
+              width={320}
+              height={320}
+            />
             <h3 className="mb-0 text-xl font-bold text-black">Flashcard Tool</h3>
             <p className="text-gray-700">Create and Revise flashcards from your notes.</p>
           </div>
-          <div className="text-center">
-            <div className="mx-auto">
-              <Image
-                src="https://raw.githubusercontent.com/joshiebudd/notionwidgets/main/essayplans_ben.webp"
-                alt="Essay plans demo"
-                width={320} // Set your desired width here
-                height={320} // Set your desired height here
-              />
-            </div>
+          <div className="flex flex-col items-center">
+            <Image
+              src="https://raw.githubusercontent.com/joshiebudd/notionwidgets/main/essayplans_ben.webp"
+              alt="Essay plans demo"
+              width={320}
+              height={320}
+            />
             <h3 className="mb-0 text-xl font-bold text-black">AI Generation Tools</h3>
             <p className="text-gray-500">Use AI to generate Essay Plans, Summaries, and more!</p>
           </div>
