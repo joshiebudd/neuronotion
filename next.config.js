0cbfd7fb81ff5156ea733d43198b79109c58e3b5
joshiebudd <140086428+joshiebudd@users.noreply.github.com>
2023-12-11 13:14:38 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,22 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
-
-// next.config.js
-
-module.exports = {
-  reactStrictMode: true,
   images: {
     domains: [
       'github.com',
       'raw.githubusercontent.com', // Add the domain without the protocol
     ],
     remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'raw.githubusercontent.com',
-        },
+      {
+        protocol: 'https',
+        hostname: 'raw.githubusercontent.com',
+      },
+    ],
   },
   vercelAnalyticsId: 'prj_XPGFnBtOVBZufVUGGE8tAiatzOtj',
 };
