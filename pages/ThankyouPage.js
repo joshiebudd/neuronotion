a826a6828b99197ddd2914ed548c91e8a9ab595e
Josh Budd <joshiebudd@gmail.com>
2023-12-11 11:26:31 +0000

    added ty page

---
+++ pages/ThankyouPage.js
@@ -0,0 +1,22 @@
+import React, { useEffect } from 'react';
+import HeaderSection from '../components/HeaderSection';
+import FooterSection from '../components/FooterSection';
+import ThankYouSection from '../components/ThankyouSection';
+
+
+const ThankYouPage = () => {
+    useEffect(() => {
+      
+    }, []); // Empty dependency array to run the effect only once
+  
+    
+    return (
+      <>
+        <HeaderSection />
+        <ThankYouSection />
+        <FooterSection />
+        </>
+  );
+}
+
+export default IndexPage;
