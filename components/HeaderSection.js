08e8a1c3cc1cc090383b7ba0c1c8e4cb922636d3
joshiebudd <140086428+joshiebudd@users.noreply.github.com>
2023-12-11 14:48:33 +0000

    Update HeaderSection.js

--- components/HeaderSection.js
+++ components/HeaderSection.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Image from 'next/image';
 
 
@@ -27,43 +27,22 @@ const HeaderSection = () => {
         }
       };
 
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-    const toggleMenu = () => {
-        setIsMenuOpen(!isMenuOpen);
-    };
-    
-  return (
+    return (
         <header>
             <div className="full-header fixed top-0 left-0 right-0 bg-white lg:bg-opacity-30 lg:backdrop-blur-lg" style={{ backdropFilter: 'blur(10px)' }}>
                 <nav className="px-8 py-5 lg:px-6">
                     <div className="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between">
-                        {/* Logo and Title - Hidden on small screens */}
-                        <div className={`flex items-center ${isMenuOpen ? 'hidden' : ''}`}>
-                            <img src="logo.webp" className="mr-3 h-6 sm:h-9 hidden lg:block" alt="Logo" />
-                            <span className="self-center whitespace-nowrap text-2xl font-semibold text-black hidden lg:block">Neuro Notion</span>
+                        <a href="#hero" className="flex items-center">
+                            <img src="https://raw.githubusercontent.com/joshiebudd/notionwidgets/main/logo.webp" className="mr-3 h-6 sm:h-9" alt="NeuroNotes Logo" />
+                            <span className="self-center whitespace-nowrap text-2xl font-semibold text-black">Neuro Notion</span>
+                        </a>
+                        <div className="hidden lg:flex items-center space-x-4">
+                            <a onClick={() => scrollToMiddle('features')}    className="px-2 py-1 text-lg font-thin text-black hover:text-gray-600 cursor-pointer hover:underline">Features</a>
+                            <a onClick={() => scrollToMiddle('pricing')}    className="px-2 py-1 text-lg font-thin text-black hover:text-gray-600 cursor-pointer hover:underline">Pricing</a>
+                            <a onClick={() => scrollToMiddle('faqs')}    className="px-2 py-1 text-lg font-thin text-black hover:text-gray-600 cursor-pointer hover:underline">FAQs</a>
                         </div>
-
-                        {/* Navigation Links - Hidden on small screens */}
-                        <div className={`hidden lg:flex items-center space-x-4 ${isMenuOpen ? 'hidden' : ''}`}>
-                            {/* Existing links... */}
-                        </div>
-
-                        {/* Get Now Button - Smaller on small screens */}
-                        <a href="#pricing" onClick={handleButtonClick} className={`StandardCheckoutButton inline-block rounded-lg bg-black px-4 py-2 text-sm font-bold text-white hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 lg:px-8 lg:py-4 lg:text-lg ${isMenuOpen ? 'hidden' : ''}`}>Get Now</a>
-
-                        {/* Burger Icon - Visible only on small screens */}
-                        <button className="lg:hidden" onClick={toggleMenu}>
-                            <img src="burger-icon.svg" alt="Menu" />
-                        </button>
+                        <a href="#pricing" onClick={handleButtonClick} className="StandardCheckoutButton inline-block rounded-lg bg-black px-8 py-4 text-lg font-bold text-white hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300">Get Now</a>
                     </div>
-
-                    {/* Mobile Menu */}
-                    {isMenuOpen && (
-                        <div className="mobile-menu lg:hidden">
-                            {/* Mobile menu content... */}
-                        </div>
-                    )}
                 </nav>
             </div>
         </header>
