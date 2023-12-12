c0aa92638c7bf53ea1797291e49715e4c29e1c0e
joshiebudd <joshiebudd@gmail.com>
2023-12-12 22:05:45 +0000

    ali updates 9

--- pages/_app.js
+++ pages/_app.js
@@ -9,20 +9,22 @@ function MyApp({ Component, pageProps }) {
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Neuro Notion ┃ The Ultimate ADHD Study Notion Template</title>
+        <link rel="dns-prefetch" href="https://cdn.jsdelivr.net" />
         <link rel="dns-prefetch" href="https://connect.facebook.net" />
         <link rel="dns-prefetch" href="https://img.icons8.com" />
         <link rel="dns-prefetch" href="https://raw.githubusercontent.com" />
         <link rel="dns-prefetch" href="https://super.so/icon/dark/check.svg" />
+        <link rel="preconnect" href="https://cdn.jsdelivr.net" />
         <link rel="preconnect" href="https://connect.facebook.net" />
         <link rel="preconnect" href="https://img.icons8.com" />
         <link rel="preconnect" href="https://raw.githubusercontent.com" />
         <link rel="preconnect" href="https://super.so/icon/dark/check.svg" />
 
-        <link rel="preload" href="https://raw.githubusercontent.com/joshiebudd/notionwidgets/main/tailwind.min.css" as="style" />
+        <link rel="preload" href="https://cdn.jsdelivr.net/npm/tailwindcss@2.0.1/dist/tailwind.min.css" as="style" />
         <link rel="icon" href="https://raw.githubusercontent.com/joshiebudd/notionwidgets/main/logo.webp" type="image/webp" />
         <link rel="preload" href="https://raw.githubusercontent.com/joshiebudd/notionwidgets/main/logo.webp" as="image" />
         <link rel="preload" href="https://raw.githubusercontent.com/joshiebudd/notionwidgets/main/maindemo.webp" as="image" />   
-        <link rel="stylesheet" href="https://raw.githubusercontent.com/joshiebudd/notionwidgets/main/tailwind.min.css" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tailwindcss@2.0.1/dist/tailwind.min.css" />
         <style>
           {`.hero-section{margin-bottom:50px;} .star-rating{margin-bottom:50px;} .centered-image{display:block;margin-left:auto;margin-right:auto;max-width:100%;height:auto;} .full-header{z-index:10;} h3{color:black;}`}
         </style>
