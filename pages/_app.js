7e4d9760b4faa3b2f113a3a72d112bc9731e264d
joshiebudd <joshiebudd@gmail.com>
2023-12-12 22:19:39 +0000

    ali updates 10

--- pages/_app.js
+++ pages/_app.js
@@ -5,29 +5,37 @@ import Script from 'next/script';
 function MyApp({ Component, pageProps }) {
   return (
     <>
+    
+          <Script
+        id="track-pageview-pixel"
+        src="https://connect.facebook.net/en_US/fbevents.js"
+        onLoad={() => {
+          fbq('init', '230622039592089');
+          fbq('track', 'PageView');
+        }}
+      />
+        
+      <Script
+        id="vercel-speed-insights"
+        src="/_vercel/insights/script.js"
+        />
+          
       <Head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Neuro Notion ┃ The Ultimate ADHD Study Notion Template</title>
-        <link rel="dns-prefetch" href="https://cdn.jsdelivr.net" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tailwindcss@2.0.1/dist/tailwind.min.css" />
+        <link rel="icon" href="https://raw.githubusercontent.com/joshiebudd/notionwidgets/main/logo.webp" type="image/webp" />
+        <style>
+          {.hero-section{margin-bottom:50px;} .star-rating{margin-bottom:50px;} .centered-image{display:block;margin-left:auto;margin-right:auto;max-width:100%;height:auto;} .full-header{z-index:10;} h3{color:black;}}
+        </style>
         <link rel="dns-prefetch" href="https://connect.facebook.net" />
         <link rel="dns-prefetch" href="https://img.icons8.com" />
-        <link rel="dns-prefetch" href="https://raw.githubusercontent.com" />
-        <link rel="dns-prefetch" href="https://super.so/icon/dark/check.svg" />
-        <link rel="preconnect" href="https://cdn.jsdelivr.net" />
         <link rel="preconnect" href="https://connect.facebook.net" />
-        <link rel="preconnect" href="https://img.icons8.com" />
-        <link rel="preconnect" href="https://raw.githubusercontent.com" />
-        <link rel="preconnect" href="https://super.so/icon/dark/check.svg" />
-
         <link rel="preload" href="https://cdn.jsdelivr.net/npm/tailwindcss@2.0.1/dist/tailwind.min.css" as="style" />
-        <link rel="icon" href="https://raw.githubusercontent.com/joshiebudd/notionwidgets/main/logo.webp" type="image/webp" />
         <link rel="preload" href="https://raw.githubusercontent.com/joshiebudd/notionwidgets/main/logo.webp" as="image" />
         <link rel="preload" href="https://raw.githubusercontent.com/joshiebudd/notionwidgets/main/maindemo.webp" as="image" />   
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tailwindcss@2.0.1/dist/tailwind.min.css" />
-        <style>
-          {`.hero-section{margin-bottom:50px;} .star-rating{margin-bottom:50px;} .centered-image{display:block;margin-left:auto;margin-right:auto;max-width:100%;height:auto;} .full-header{z-index:10;} h3{color:black;}`}
-        </style>
+        
       </Head>
 
 
