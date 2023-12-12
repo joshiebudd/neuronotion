e689ef94498d2af4afecbdce6f2be9ccc3a25171
joshiebudd <140086428+joshiebudd@users.noreply.github.com>
2023-12-12 14:11:23 +0000

    Update HeaderSection.js

--- components/HeaderSection.js
+++ components/HeaderSection.js
@@ -32,7 +32,8 @@ const HeaderSection = () => {
             <div className="full-header fixed top-0 left-0 right-0 bg-white lg:bg-opacity-30 lg:backdrop-blur-lg" style={{ backdropFilter: 'blur(10px)' }}>
                 <nav className="px-8 py-5 lg:px-6">
                     <div className="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between">
-	@@ -37,11 +58,11 @@ const HeaderSection = () => {
+                        <a href="#hero" className="flex items-center">
+                            <img src="https://raw.githubusercontent.com/joshiebudd/notionwidgets/main/logo.webp" className="mr-3 h-6 sm:h-9" alt="NeuroNotes Logo" />
                             <span className="self-center whitespace-nowrap text-2xl font-semibold text-black">Neuro Notion</span>
                         </a>
                         <div className="hidden lg:flex items-center space-x-4">
@@ -40,7 +41,12 @@ const HeaderSection = () => {
                             <a onClick={() => scrollToMiddle('pricing')}    className="px-2 py-1 text-lg font-thin text-black hover:text-gray-600 cursor-pointer hover:underline">Pricing</a>
                             <a onClick={() => scrollToMiddle('faqs')}    className="px-2 py-1 text-lg font-thin text-black hover:text-gray-600 cursor-pointer hover:underline">FAQs</a>
                         </div>
-<a href="#pricing" onClick={handleButtonClick} className="StandardCheckoutButton inline-block rounded-lg bg-black px-4 py-2 text-sm font-bold text-white hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 sm:px-8 sm:py-4 sm:text-lg">Get Now</a>
+                        <a href="#pricing" onClick={handleButtonClick} className="StandardCheckoutButton inline-block rounded-lg bg-black px-8 py-4 text-lg font-bold text-white hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300">Get Now</a>
                     </div>
                 </nav>
             </div>
+        </header>
+    );
+};
+
+export default HeaderSection;
