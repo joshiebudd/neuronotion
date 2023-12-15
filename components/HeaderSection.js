2db022d4e79b44b629d60e03c0d25d416646cf87
joshiebudd <joshiebudd@gmail.com>
2023-12-15 21:00:28 +0000

    Revert "header"
    This reverts commit cf8368516c2ef9a84f1f066517f878485ca72098.

--- components/HeaderSection.js
+++ components/HeaderSection.js
@@ -52,25 +52,23 @@ const HeaderSection = () => {
             <SpeedInsights />
           
             <div className="full-header fixed top-0 left-0 right-0 bg-white lg:bg-opacity-30 lg:backdrop-blur-lg" style={{ backdropFilter: 'blur(10px)' }}>
-    <nav className="px-8 py-5 lg:px-6">
-        <div className="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between">
-            <a href="#hero" className="flex items-center">
-                {/* Increase the logo size */}
-                <Image width={56} height={56} loading="eager" className="mr-3 sm:h-9" src="https://raw.githubusercontent.com/joshiebudd/notionwidgets/main/logo.webp" alt="NeuroNotes Logo" />
-                {/* Make the title smaller on small and medium screens */}
-                <span className="self-center whitespace-nowrap text-xl sm:text-2xl lg:text-2xl font-semibold text-black">Neuro Notion</span>
-            </a>
-            <div className="hidden lg:flex items-center space-x-4">
-                <a onClick={() => scrollToMiddle('features')} className="px-2 py-1 text-base font-thin text-black hover:text-gray-600 cursor-pointer hover:underline">Features</a>
-                <a onClick={() => scrollToMiddle('pricing')} className="px-2 py-1 text-base font-thin text-black hover:text-gray-600 cursor-pointer hover:underline">Pricing</a>
-                <a onClick={() => scrollToMiddle('faqs')} className="px-2 py-1 text-base font-thin text-black hover:text-gray-600 cursor-pointer hover:underline">FAQs</a>
+                <nav className="px-8 py-5 lg:px-6">
+                    <div className="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between">
+                        <a href="#hero" className="flex items-center">
+                            {/* Increase the logo size */}
+                            <Image width={56} height={56} loading="eager" className="mr-3 sm:h-9" src="https://raw.githubusercontent.com/joshiebudd/notionwidgets/main/logo.webp" alt="NeuroNotes Logo" />
+                            {/* Make the title smaller on small and medium screens */}
+                            <span className="self-center whitespace-nowrap text-xl sm:text-2xl lg:text-2xl font-semibold text-black">Neuro Notion</span>
+                        </a>
+                        <div className="hidden lg:flex items-center space-x-4">
+                            <a onClick={() => scrollToMiddle('features')} className="px-2 py-1 text-lg font-thin text-black hover:text-gray-600 cursor-pointer hover:underline">Features</a>
+                            <a onClick={() => scrollToMiddle('pricing')} className="px-2 py-1 text-lg font-thin text-black hover:text-gray-600 cursor-pointer hover:underline">Pricing</a>
+                            <a onClick={() => scrollToMiddle('faqs')} className="px-2 py-1 text-lg font-thin text-black hover:text-gray-600 cursor-pointer hover:underline">FAQs</a>
+                        </div>
+                        <a href="#pricing" onClick={handleButtonClick} className="inline-block rounded-lg bg-black px-4 py-2 text-lg font-bold text-white hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 lg:px-8 lg:py-4">Buy Now</a>
+                    </div>
+                </nav>
             </div>
-            <div className="flex-grow"></div> {/* Added flex-grow to push the button to the right */}
-            <a href="#pricing" onClick={handleButtonClick} className="inline-block rounded-lg bg-black px-4 py-2 text-base font-bold text-white hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 lg:px-8 lg:py-4">Buy Now</a>
-        </div>
-    </nav>
-</div>
-
         </header>
     );
 };
