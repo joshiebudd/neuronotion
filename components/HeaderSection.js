ff758b3437c51910e51a7c694a6c7cf6fc62a3ac
joshiebudd <140086428+joshiebudd@users.noreply.github.com>
2023-12-15 20:48:27 +0000

    burger for mobile

--- components/HeaderSection.js
+++ components/HeaderSection.js
@@ -1,4 +1,6 @@
-import React, { useEffect } from 'react';
+import React, { useState } from 'react';
+import { GiHamburgerMenu } from "react-icons/gi";
+import { RxCross2 } from "react-icons/rx";
 import { SpeedInsights } from "@vercel/speed-insights/react";
 import Image from 'next/image';
 import Script from 'next/script';
@@ -28,9 +30,18 @@ const HeaderSection = () => {
         }
     };
 
+
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const handleMenuToggle = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
+
+
     return (
         <header>
-            <Script 
+            <Script
                 id="fb-pixel-script"
                 strategy="afterInteractive"
                 dangerouslySetInnerHTML={{
@@ -43,34 +54,42 @@ const HeaderSection = () => {
                         t.src=v;s=b.getElementsByTagName(e)[0];
                         s.parentNode.insertBefore(t,s)}(window, document,'script',
                         'https://connect.facebook.net/en_US/fbevents.js');
-                        fbq('init', '230622039592089'); // Replace with your actual pixel ID
+                        fbq('init', '230622039592089'); // Insert your pixel ID here
                         fbq('track', 'PageView');
                     `,
                 }}
             />
-
             <SpeedInsights />
-          
-            <div className="full-header fixed top-0 left-0 right-0 bg-white lg:bg-opacity-30 lg:backdrop-blur-lg" style={{ backdropFilter: 'blur(10px)' }}>
-    <nav className="px-8 py-5 lg:px-6">
-        <div className="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between">
-            <a href="#hero" className="flex items-center">
-                {/* Increase the logo size */}
-                <Image width={56} height={56} loading="eager" className="mr-3 sm:h-9" src="https://raw.githubusercontent.com/joshiebudd/notionwidgets/main/logo.webp" alt="NeuroNotes Logo" />
-                {/* Make the title smaller on small and medium screens */}
-                <span className="self-center whitespace-nowrap text-xl sm:text-2xl lg:text-2xl font-semibold text-black">Neuro Notion</span>
-            </a>
-            <div className="hidden lg:flex items-center space-x-4">
-                <a onClick={() => scrollToMiddle('features')} className="px-2 py-1 text-base font-thin text-black hover:text-gray-600 cursor-pointer hover:underline">Features</a>
-                <a onClick={() => scrollToMiddle('pricing')} className="px-2 py-1 text-base font-thin text-black hover:text-gray-600 cursor-pointer hover:underline">Pricing</a>
-                <a onClick={() => scrollToMiddle('faqs')} className="px-2 py-1 text-base font-thin text-black hover:text-gray-600 cursor-pointer hover:underline">FAQs</a>
-            </div>
-            <div className="flex-grow"></div> {/* Added flex-grow to push the button to the right */}
-            <a href="#pricing" onClick={handleButtonClick} className="inline-block rounded-lg bg-black px-4 py-2 text-base font-bold text-white hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 lg:px-8 lg:py-4">Buy Now</a>
-        </div>
-    </nav>
-</div>
+            <div className={`fixed top-0 left-0 right-0 z-40 w-full bg-white p-5 transition duration-500 ease-in-out ${isMenuOpen ? 'h-screen' : 'h-20'}`}>
+                <nav className="flex items-center justify-between">
+                    <a href="#hero" className="flex items-center">
+                        <Image
+                            src="https://raw.githubusercontent.com/joshiebudd/notionwidgets/main/logo.webp"
+                            alt="Neuro Notion logo"
+                            width={56}
+                            height={56}
+                            className="mr-3"
+                        />
+                        <span className="text-xl font-bold">Neuro Notion</span>
+                    </a>
+                    <button onClick={handleMenuToggle} className="text-black lg:hidden">
+                        {isMenuOpen ? (
+                            // Here you can put an icon for closing the menu
+                            <RxCross2 className="h-6 w-6" />
+                        ) : (
+                            // This is the hamburger menu icon
+                            <GiHamburgerMenu className="h-6 w-6" />
+                        )}
+                    </button>
 
+                </nav>
+                <div className={`flex flex-col mt-4 ${isMenuOpen ? 'block' : 'hidden'} lg:hidden`}>
+                    <a onClick={() => scrollToMiddle('features')} className="mb-2 text-base font-thin hover:underline">Features</a>
+                    <a onClick={() => scrollToMiddle('pricing')} className="mb-2 text-base font-thin hover:underline">Pricing</a>
+                    <a onClick={() => scrollToMiddle('faqs')} className="mb-2 text-base font-thin hover:underline">FAQs</a>
+                    <a href="#pricing" onClick={handleButtonClick} className="self-center bg-black px-6 py-2 text-base font-bold text-white rounded-lg hover:bg-opacity-90 focus:outline-none">Buy Now</a>
+                </div>
+            </div>
         </header>
     );
 };
