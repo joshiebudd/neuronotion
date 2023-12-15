f77a6f23967fcad2ec5ec72f55b4d03e143538d9
joshiebudd <140086428+joshiebudd@users.noreply.github.com>
2023-12-15 20:21:55 +0000

    hero

--- components/HeroSection.js
+++ components/HeroSection.js
@@ -17,7 +17,7 @@ const HeroSection = () => {
   return (
     <section id="hero" className="bg-gray-50 hero-section pt-32 sm:pt-28 md:pt-30 lg:pt-32">
       <div className="mx-auto max-w-screen-xl px-4 py-8 text-center lg:px-12 lg:py-16">
-        <h1 className="mb-10 font-bold leading-none tracking-tight text-black text-5xl">
+        <h1 className="mb-10 font-bold leading-none tracking-tight text-black text-4xl">
           Silence the ADHD Chaos. <br /> Become 3x more productive.
         </h1>
         <p className="mb-10 text-lg font-normal text-black sm:px-18 lg:text-xl xl:px-48">
@@ -34,7 +34,7 @@ const HeroSection = () => {
 
         {/* Stars */}
         <div className="mb-2 flex justify-center">
-          <span className="mt-3 mb-0 text-5xl leading-none text-black">★★★★★</span>
+          <span className="mt-3 mb-0 text-3xl leading-none text-black">★★★★★</span>
         </div>
         <span className="mt-0 mb-0 text-sm text-gray-800">
           4.82/5 from 1,300+ Students with ADHD.
