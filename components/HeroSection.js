51442e1203ed818b5cd6035ab2679718e820ec38
joshiebudd <joshiebudd@gmail.com>
2024-02-01 12:37:19 +0000

    style(landing)

--- components/HeroSection.js
+++ components/HeroSection.js
@@ -14,13 +14,12 @@ const HeroSection = () => {
   return (
     <section id="hero" className="bg-gray-50 hero-section pt-24 sm:pt-32">
       <div className="mx-auto max-w-screen-xl px-4 py-8 text-center lg:px-12 lg:py-16">
-        <h1 className="mb-10 font-bold leading-none tracking-tight text-black text-4xl">
-          Silence the ADHD Chaos. <br /> 3x your Productivity.
+        <h1 className="mb-10 font-bold leading-none tracking-tight text-black text-5xl">
+          💤 Silence the ADHD Chaos. <br /> 3x your Productivity 🧠
         </h1>
         <p className="mb-10 text-xl font-normal text-black sm:px-18 xl:px-48 mt-6">
-          Discover Clarity and Control with Neuro Notion. Your ultimate ADHD
-          study partner which streamlines resources and seamlessly organizes
-          your workflow.
+          Block out the noise with Neuro Notion: The Ultimate ADHD Study Template.
+          Enter a distraction free zone, with all your study needs in one place. 
         </p>
 
         <div className="mt-4 flex justify-center space-x-4">
