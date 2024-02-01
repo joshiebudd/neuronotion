86e6de20104d23122b6e6dd47571d0c4b622221a
joshiebudd <joshiebudd@gmail.com>
2024-02-01 14:59:10 +0000

    style(123)

--- components/Benefits/Benefits1.js
+++ components/Benefits/Benefits1.js
@@ -9,7 +9,7 @@ const Benefits1 = () => {
       <div className="py-8 pb-8 sm:pb-24 px-4 mx-auto max-w-screen-xl sm:pt-4 lg:px-12">
         <div className="max-w-screen-md mx-auto text-center mb-2 mt-2">
           <h2 className="text-4xl tracking-tight font-bold text-gray-900">
-          😌 Block out the noise  
+          Block out the noise  
             <br />and channel the flow state ✨
           </h2>
           <p className="mt-6 text-gray-700 text-xl text-gray-900">
@@ -19,7 +19,7 @@ const Benefits1 = () => {
         </div>
         <div className="gap-x-4 lg:gap-y-2 flex items-center mt-16 flex-wrap md:flex-nowrap">
 
-          <div className="flex items-center text-center justify-center mb-2 mt-2 mx-12 order-1 md:order-1 w-full min-w-[200px] md:w-fit">
+          <div className="flex items-center text-center justify-center mb-2 mt-2 mx-12 order-2 md:order-1 w-full min-w-[200px] md:w-fit">
             <Image
               src="/benefit1.webp"
               alt="More Hyper-focus, Less Distractions"
@@ -29,7 +29,7 @@ const Benefits1 = () => {
 
 
           {/* begin - description */}
-          <div className="max-w-[600px] mx-12 flex flex-col items-center mb-12 order-2 md:mb-0 md:order-2 lg:items-start lg:mx-4">
+          <div className="max-w-[600px] mx-12 flex flex-col items-center mb-12 order-1 md:mb-0 md:order-2 lg:items-start lg:mx-4">
             <div>
               <div className="flex items-center">
                 <UpArrowIcon color="#15803d" />
