eed2fe10b3061b301f010c2b60fa3208ff2f0673
joshiebudd <joshiebudd@gmail.com>
2024-02-08 12:58:19 +0000

    style(scripts)

--- components/tracking.js
+++ components/tracking.js
@@ -1,8 +1,8 @@
 import { track } from "@vercel/analytics";
-let scrolled = false;
 let reachedBottom = false;
 let loadTime = new Date(),
   unloadTime;
+
 export function setupBeforeUnload() {
   window.addEventListener("beforeunload", function (event) {
     unloadTime = new Date();
@@ -13,31 +13,19 @@ export function setupBeforeUnload() {
     track(`Visited Time: ${visitedTime}`);
   });
 }
+
 export function setupLoad() {
   window.addEventListener("DOMContentLoaded", function (event) {
     track("Visited");
     loadTime = new Date();
   });
 }
+
 export function setupScroll() {
   window.addEventListener("scroll", function (event) {
-    if (!scrolled) {
-      track("Scrolled");
-      scrolled = true;
-    }
-    if (
-      !reachedBottom &&
-      this.scrollY > document.getElementById("footer").offsetTop - innerHeight
-    ) {
+    if (!reachedBottom && this.scrollY > document.getElementById("footer").offsetTop - innerHeight) {
       reachedBottom = true;
       track("Reached Bottom");
-    }
-    const pricingSection = this.document.getElementById("pricing");
-    if (
-      this.scrollY > pricingSection.clientTop &&
-      this.scrollY < pricingSection.clientHeight + pricingSection.clientTop
-    ) {
-      track("Leave at Price Section.");
     }
   });
 }
