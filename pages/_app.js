3c481b51242a3a10915550c9b2fc4dbc1fde882a
joshiebudd <joshiebudd@gmail.com>
2024-02-26 17:57:38 +0000

    style(posthog)

--- pages/_app.js
+++ pages/_app.js
@@ -8,12 +8,17 @@ import { PostHogProvider } from "posthog-js/react";
 
 // Check that PostHog is client-side (used to handle Next.js SSR)
 if (typeof window !== "undefined") {
-  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
-    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://app.posthog.com",
-    loaded: (posthog) => {
-      if (process.env.NODE_ENV === "development") posthog.debug();
-    },
-  });
+  posthog.init(
+    process.env.NEXT_PUBLIC_POSTHOG_KEY ||
+      "phc_3lTf840WFEVTY07GoU20Happ4w4r4YZLpeZuzwVWd7o",
+    {
+      api_host:
+        process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://app.posthog.com",
+      loaded: (posthog) => {
+        if (process.env.NODE_ENV === "development") posthog.debug();
+      },
+    }
+  );
 }
 
 function MyApp({ Component, pageProps }) {
--- pages/index.js
+++ pages/index.js
@@ -15,6 +15,18 @@ import FinalCTA from "../components/FinalCTASection";
 import FooterSection from "../components/FooterSection";
 import ToTop from "../components/Common/ToTop";
 import BlazingSection from "../components/BlazingSection.js";
+
+// Check that PostHog is client-side (used to handle Next.js SSR)
+if (typeof window !== "undefined") {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://app.posthog.com",
+    // Enable debug mode in development
+    loaded: (posthog) => {
+      if (process.env.NODE_ENV === "development") posthog.debug();
+    },
+  });
+}
+
 import {
   setupBeforeUnload,
   setupLoad,
