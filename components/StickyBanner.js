04c18e074cdeb89f97c4e1274152f9943fc8b1f6
joshiebudd <joshiebudd@gmail.com>
2024-03-06 12:11:56 +0000

    style(fix)

--- components/StickyBanner.js
+++ components/StickyBanner.js
@@ -13,14 +13,14 @@ const StickyBanner = () => {
             <span className="sr-only">Discount</span>
           </span>
           <span className="text-white">
-            Anniversary Sale! <br /> Use <span className="font-bold underline">&quot;OFF50&quot;</span> at <br /> checkout for <span className="font-bold underline">50% off</span>.
+            Anniversary Sale! <br /> Use <span className="font-bold underline">"OFF50"</span> at <br /> checkout for <span className="font-bold underline">50% off</span>.
           </span>
         </p>
       </div>
       <div className="flex items-center">
-        <a href="#pricing" className="flex items-center text-sm font-medium text-red hover:underline mr-4">
+        <span className="flex items-center text-sm font-medium text-red hover:underline mr-4">
           Valid Today Only!
-        </a>
+        </span>
         <button type="button" className="flex-shrink-0 inline-flex justify-center w-7 h-7 items-center text-gray-400 hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 dark:hover:bg-gray-600 dark:hover:text-white">
           <svg className="w-3 h-3" aria-hidden="true" fill="none" viewBox="0 0 14 14">
             <path
