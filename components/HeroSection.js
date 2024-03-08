fea5166adfcbac04971f03d1f78c25a1c59c05f8
joshiebudd <joshiebudd@gmail.com>
2024-03-08 11:13:56 +0000

    style(buttonswap)

--- components/HeroSection.js
+++ components/HeroSection.js
@@ -21,13 +21,6 @@ const HeroSection = () => {
           Neuro Notion is an all-in-one Notion system for students which blocks out the ADHD, letting you enter flow state, on demand.
         </p>
         <div className="mt-4 flex justify-center space-x-4">
-          {/* See Demo Button */}
-          <a
-            href="#demo"
-            className="inline-block rounded-lg bg-gray-700 px-8 py-4 text-lg font-bold text-white hover:bg-gray-600 focus:outline-none focus:ring-4 focus:ring-gray-300"
-          >
-            See Demo
-          </a>
           {/* Get NeuroNotes Button */}
           <a
             href="#pricing"
@@ -35,6 +28,13 @@ const HeroSection = () => {
             className="StandardCheckoutButton inline-block rounded-lg bg-black px-8 py-4 text-lg font-bold text-white hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300"
           >
             Buy Now
+          </a>
+          {/* See Demo Button */}
+          <a
+            href="#demo"
+            className="inline-block rounded-lg bg-gray-700 px-8 py-4 text-lg font-bold text-white hover:bg-gray-600 focus:outline-none focus:ring-4 focus:ring-gray-300"
+          >
+            See Demo
           </a>
         </div>
         {/* Stars */}
