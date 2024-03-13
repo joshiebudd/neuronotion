c95b7815030e8915571f4158d07e0959f7285f5c
joshiebudd <joshiebudd@gmail.com>
2024-03-13 17:49:16 +0000

    style(stick)

--- pages/stick.js
+++ pages/stick.js
@@ -1,25 +1,28 @@
-import React from 'react';
-import Head from 'next/head';
-import HeaderSection from '../components/HeaderSection';
-import FooterSection from '../components/FooterSection';
-import LongFormVideo from '../components/LongFormVideo';
+import React, { useEffect } from "react";
+import Head from "next/head";
+import HeaderSection from "../components/HeaderSection";
+import FooterSection from "../components/FooterSection";
+import LongFormVideo from "../components/LongFormVideo";
 
 const HomePage = () => {
+  useEffect(() => {
+    // Any necessary effects or tracking logic can be added here
+  }, []);
+
   return (
-    <div>
+    <>
       <Head>
-        <title>Neuro Notion </title>
-        <meta name="description" content="Watch a long form video to learn how to do XYZ" />
+        <title>Neuro Notion</title>
+        <meta
+          name="description"
+          content="Watch a long form video to learn how to do XYZ"
+        />
       </Head>
 
       <HeaderSection />
-
-      <main className="container mx-auto py-8">
-        <LongFormVideo />
-      </main>
-
+      <LongFormVideo />
       <FooterSection />
-    </div>
+    </>
   );
 };
 
