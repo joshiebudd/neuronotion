7d633d4941efab4a483bbad624b02cd6eafb9cfc
joshiebudd <joshiebudd@gmail.com>
2024-03-13 18:12:00 +0000

    style(stick)

--- components/LongFormVideo.js
+++ components/LongFormVideo.js
@@ -1,41 +1,40 @@
 import React from 'react';
 
-const VideoSection = () => {
-  // Assuming the video URL and thumbnail are passed as props, or can be imported
-  const videoUrl = 'https://NeuroNotionPullZonw.b-cdn.net/ProductDemoFinal.mp4';
-  const thumbnailUrl = 'https://raw.githubusercontent.com/joshiebudd/neuronotion/main/public/thumbnailfordemo.png';
-  
-  // This function could be used to handle any interactions like analytics events
-  const handlePlayButtonClick = () => {
-    // Analytics event logic can be implemented here
+const VideoPlayer = () => {
+  let videoRef = React.createRef();
+
+  const playVideo = () => {
+    if (videoRef.current) {
+      videoRef.current.play();
+    }
   };
 
   return (
-    <div className="bg-white p-4 rounded-lg shadow-lg">
-      <div className="mb-4">
-        {/* Video thumbnail with play button */}
-        <div className="relative cursor-pointer" onClick={handlePlayButtonClick}>
-          <video
-            className="w-full rounded-lg"
-            controls
-            poster={thumbnailUrl}
+    <div className="component1 p-4 flex justify-center items-center">
+      <div className="relative group cursor-pointer" onClick={playVideo}>
+        <video
+          ref={videoRef}
+          className="max-w-full h-auto rounded-md shadow-lg"
+          controls={false}
+          preload="none"
+          poster="YOUR_POSTER_URL" // Replace with your video poster url
+        >
+          <source src="YOUR_VIDEO_URL" type="video/mp4" /> {/* Replace with your video url */}
+          Your browser does not support the video tag.
+        </video>
+        <div className="absolute top-0 right-0 bottom-0 left-0 flex justify-center items-center">
+          <div
+            className="play-button flex justify-center items-center w-16 h-16 bg-gray-800 bg-opacity-50 rounded-full group-hover:bg-opacity-75 transition duration-150 ease-in-out"
+            onClick={playVideo}
           >
-            <source src={videoUrl} type="video/mp4" />
-            Your browser does not support the video tag.
-          </video>
-          <div className="absolute top-0 right-0 bottom-0 left-0 flex items-center justify-center">
-            <svg className="h-12 w-12 text-gray-800" fill="currentColor" viewBox="0 0 84 84">
-              <circle opacity="0.9" cx="42" cy="42" r="42" fill="white" />
-              <path d="M55.891 42.752l-22.23 12.564V30.189z" />
+            <svg className="w-10 h-10 text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+              <path d="M8 5v14l11-7z" />
             </svg>
           </div>
         </div>
       </div>
-      <p className="text-gray-600 text-sm">
-        WATCH THIS LONG FORM VIDEO BELOW TO FIND OUT HOW TO DO XYZ. REPLACE COPY.
-      </p>
     </div>
   );
 };
 
-export default VideoSection;
+export default VideoPlayer;
