24466f3eb968148d8a04fb4e09d7b45ad1a23e00
joshiebudd <joshiebudd@gmail.com>
2024-03-15 13:17:53 +0000

    style(marg)

--- components/LongVideo.js
+++ components/LongVideo.js
@@ -5,12 +5,12 @@ const LongVideo = () => {
   return (
     <section className="px-4 bg-gray-100 pt-36 sm:pt-32">
       <div className="text-center">
-        <div className="flex justify-center pb-12">
+        <div className="flex justify-center pb-6">
           <h2 className="md:max-w-[600px]  text-2xl md:text-4xl  font-bold leading-6 text-center ">
           Make ADHD your Superpower with a Study Focus System
           </h2>
         </div>
-        <div className="timer-widget flex justify-center height-auto pt-4 pb-16">
+        <div className="timer-widget flex justify-center height-auto pt-4 pb-8">
           <div className="inline-block p-0 rounded-lg overflow-hidden">
             <video
               className="w-7/10 rounded-lg"
