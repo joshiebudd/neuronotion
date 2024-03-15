60c7bb82b4a1f3746765255a4c190d622f5ae750
joshiebudd <joshiebudd@gmail.com>
2024-03-15 13:38:08 +0000

    style(copy)

--- components/LongVideo.js
+++ components/LongVideo.js
@@ -1,6 +1,6 @@
 import React from "react";
 import CourseFormBeta from "./CourseFormBeta";
-import FunnelCopy from "./funnelcopy";
+import FunnelCopy from "./FunnelCopy12";
 
 const LongVideo = () => {
   return (
