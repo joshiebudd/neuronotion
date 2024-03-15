8d73b4984489add30de2d1d993130b94ab0f2f11
joshiebudd <joshiebudd@gmail.com>
2024-03-15 13:44:17 +0000

    style(breaks)

--- components/FunnelCopy12.js
+++ components/FunnelCopy12.js
@@ -4,20 +4,28 @@ const FunnelCopy = () => {
   return (
     <section className="article-section">
     <p>Hey Friend,</p>
+    <br />
 
     <p>Having ADHD should not mean you cannot thrive in life, hobbies and relationships.</p>
+    <br />
 
     <p>And it definitely should not condemn you to failing in school.</p>
+    <br />
 
     <p>As an ADHDer, I have learnt how to thrive in a world that is designed for people with neurotypical brains.</p>
+    <br />
 
     <p>And while in the past it meant that I could not study or do schoolwork as early as most people.</p>
+    <br />
 
     <p>Having a system for focus now helps me get stuff done as soon as I need to.</p>
+    <br />
 
     <p>All this while managing three businesses that I built as a student.</p>
+    <br />
 
     <p><strong>I invite you to get this guide today if…</strong></p>
+    <br />
 
     <ul>
       <li>You want a focus solution that will last longer than two weeks.</li>
@@ -30,14 +38,17 @@ const FunnelCopy = () => {
       <br />
       <li>You want to turn your ADHD hyper-focus into a superpower for your other interests…and not be worried about undone schoolwork.</li>
     </ul>
+    <br />
 
     <p><strong>What this guide will show you:</strong></p>
+    <br />
 
     <ol>
       <li>The principles that inform a lasting focus solution. We will also debunk harmful hacks that run on vibes alone but never really help you build a habit of focus.</li>
       <br />
       <li>How to build your productivity system with today&apos;s digital tools. All without needing to code. Some options only require minutes to plug and play!</li>
     </ol>
+    <br />
 
     <p>If $2 is not too costly for improving your life, fill this form below and I&apos;ll see you inside the guide.</p>
   </section>
