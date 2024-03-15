413738c33b4ed39dbba4e11ac6ed35be6177a25f
joshiebudd <joshiebudd@gmail.com>
2024-03-15 13:58:47 +0000

    style(syntax)

--- components/CourseFormAlpha.js
+++ components/CourseFormAlpha.js
@@ -1,17 +1,21 @@
-import React, { useEffect, useMemo, useState } from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import FunnelCopy from "./FunnelCopyAlpha";
 
 const CourseFormAlpha = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
   const onGetAccessClicked = () => {
     window.location.href = "https://buy.stripe.com/6oE5mvdLVcRk3vicN3";
   };
+
   return (
     <section className="bg-gray-100 pt-24 sm:pt-32">
       <div className="text-center px-4">
         <div className=" flex justify-center">
-        <h2 className="md:max-w-[600px]  text-2xl md:text-4xl  font-bold leading-6 text-center ">
-        Make ADHD your Superpower with a Study Focus System.
+          <h2 className="md:max-w-[600px] text-2xl md:text-4xl font-bold leading-6 text-center">
+            Make ADHD your Superpower with a Study Focus System.
           </h2>
         </div>
         <div className="timer-widget flex justify-center height-auto pt-4 pb-16">
@@ -26,41 +30,50 @@ const CourseFormAlpha = () => {
           />
         </div>
 
-<FunnelCopy />
+        <FunnelCopy />
 
-        <div className="sign-form sm:mx-auto sm:w-full sm:max-w-sm">
-          <div class="space-y-2">
-            <div>
-              <div class="mt-2">
-                <input
-                  id="name"
-                  name="name"
-                  placeholder="Enter Name"
-                  required
-                  class="block pl-2 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                ></input>
+        <div>
+          <div className="sign-form sm:mx-auto sm:w-full sm:max-w-sm">
+            <div className="space-y-2">
+              <div>
+                <div className="mt-2">
+                  <input
+                    id="name"
+                    name="name"
+                    autoComplete="email"
+                    placeholder="Enter Name"
+                    required
+                    className="block pl-2 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                  />
+                </div>
               </div>
-            </div>
-            <div>
-              <div class="mt-2">
-                <input
-                  id="email"
-                  name="email"
-                  type="email"
-                  placeholder="Enter Email Address"
-                  required
-                  class="block pl-2 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
+              <div>
+                <div className="mt-2 mb-4">
+                  <input
+                    id="email"
+                    name="email"
+                    type="email"
+                    placeholder="Enter Email Address"
+                    autoComplete="email"
+                    required
+                    className="block pl-2 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </div>
               </div>
-            </div>
 
-            <div className="flex justify-center">
-              <button
-                class="flex w-80 justify-center rounded-md bg-blue-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                onClick={onGetAccessClicked}
-              >
-                Get Access to Course: $1.99
-              </button>
+              <div className="flex justify-center">
+                <button
+                  type="submit"
+                  className="flex w-80 justify-center rounded-md bg-sky-800 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 mb-8"
+                  onClick={onGetAccessClicked}
+                >
+                  Get Access to Course: $1.99
+                </button>
+              </div>
             </div>
           </div>
         </div>
@@ -68,4 +81,5 @@ const CourseFormAlpha = () => {
     </section>
   );
 };
+
 export default CourseFormAlpha;
--- components/FunnelCopyBeta.js
+++ components/FunnelCopyBeta.js
@@ -3,56 +3,56 @@ import React from 'react';
 const FunnelCopyBeta = () => {
   return (
     <section className="article-section">
-    <p>Hey Friend,</p>
-    <br />
-
-    <p>Having ADHD should not mean you cannot thrive in life, hobbies and relationships.</p>
-    <br />
-
-    <p>And it definitely should not condemn you to failing in school.</p>
-    <br />
-
-    <p>As an ADHDer, I have learnt how to flourish in a world that is designed for people with neurotypical brains.</p>
-    <br />
-
-    <p>And while in the past it meant that I could not study or do schoolwork as early as most people.</p>
-    <br />
-
-    <p>Having a system for focus now helps me get stuff done as soon as I need to.</p>
-    <br />
-
-    <p>All this while managing three businesses that I built as a student.</p>
-    <br />
-    <br />
-
-    <p><strong>I invite you to get this guide today if…</strong></p>
-
-    <ul>
-      <li>You want a focus solution that will last longer than two weeks.</li>
+      <p>Hey Friend,</p>
       <br />
-      <li>You're tired of scrolling TikTok and blogs for the next trendy "productivity hack" every now and then.</li>
-      <br />
-      <li>You're fed up of trying out new "task manager apps" every other month.</li>
-      <br />
-      <li>You want to be able to focus on demand.</li>
-      <br />
-      <li>You want to turn your ADHD hyper-focus into a superpower for your other interests…and not be worried about undone schoolwork.</li>
-    </ul>
-    <br />
-    <br />
 
-    <p><strong>What this guide will show you:</strong></p>
-
-    <ol>
-      <li>The principles that inform a lasting focus solution. We will also debunk harmful hacks that run on vibes alone but never really help you build a habit of focus.</li>
+      <p>Having ADHD should not mean you cannot thrive in life, hobbies and relationships.</p>
       <br />
-      <li>How to build your productivity system with today's digital tools. All without needing to code. Some options only require minutes to plug and play!</li>
-    </ol>
-    <br />
 
-    <p>If $2 is not too costly for improving your life, fill this form below and I'll see you inside the guide.</p>
-    <br />
-  </section>
+      <p>And it definitely should not condemn you to failing in school.</p>
+      <br />
+
+      <p>As an ADHDer, I have learnt how to flourish in a world that is designed for people with neurotypical brains.</p>
+      <br />
+
+      <p>And while in the past it meant that I could not study or do schoolwork as early as most people.</p>
+      <br />
+
+      <p>Having a system for focus now helps me get stuff done as soon as I need to.</p>
+      <br />
+
+      <p>All this while managing three businesses that I built as a student.</p>
+      <br />
+      <br />
+
+      <p><strong>I invite you to get this guide today if…</strong></p>
+
+      <ul>
+        <li>You want a focus solution that will last longer than two weeks.</li>
+        <br />
+        <li>You&apos;re tired of scrolling TikTok and blogs for the next trendy &quot;productivity hack&quot; every now and then.</li>
+        <br />
+        <li>You&apos;re fed up of trying out new &quot;task manager apps&quot; every other month.</li>
+        <br />
+        <li>You want to be able to focus on demand.</li>
+        <br />
+        <li>You want to turn your ADHD hyper-focus into a superpower for your other interests…and not be worried about undone schoolwork.</li>
+      </ul>
+      <br />
+      <br />
+
+      <p><strong>What this guide will show you:</strong></p>
+
+      <ol>
+        <li>The principles that inform a lasting focus solution. We will also debunk harmful hacks that run on vibes alone but never really help you build a habit of focus.</li>
+        <br />
+        <li>How to build your productivity system with today&apos;s digital tools. All without needing to code. Some options only require minutes to plug and play!</li>
+      </ol>
+      <br />
+
+      <p>If $2 is not too costly for improving your life, fill this form below and I&apos;ll see you inside the guide.</p>
+      <br />
+    </section>
   );
 };
 
