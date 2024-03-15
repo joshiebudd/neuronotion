1b539067c327af38e9ab82da4e7a87c97ea8c618
joshiebudd <joshiebudd@gmail.com>
2024-03-15 12:35:56 +0000

    style(changes)

--- components/FreeBie.js
+++ components/FreeBie.js
@@ -30,14 +30,14 @@ const FreeBie = () => {
             <div className="flex justify-center">
               <button
                 onClick={onGetFreeBie}
-                class="flex md:w-80 w-72 justify-center rounded-md bg-sky-800 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                class="flex md:w-80 w-72 justify-center rounded-md bg-sky-800 mt-2 mb-4 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 Get Freebie
               </button>
             </div>
             <div className="pt-3 ">
-              <p className=" italic text-xl leading-10 text-gray-700">
-                No Email Required
+              <p className=" italic text-md leading-10 text-gray-700">
+                *No email required.
               </p>
             </div>
           </div>
