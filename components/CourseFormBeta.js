debf2f7c511410c383644e030b81a05583f9e3ea
joshiebudd <joshiebudd@gmail.com>
2024-03-15 12:45:25 +0000

    style(margin)

--- components/CourseFormBeta.js
+++ components/CourseFormBeta.js
@@ -25,7 +25,7 @@ const CourseFormBeta = () => {
             </div>
           </div>
           <div>
-            <div class="mt-2">
+            <div class="mt-2 mb-4">
               <input
                 id="email"
                 name="email"
