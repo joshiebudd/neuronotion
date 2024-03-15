110879017f35591f63d2959853a5a226314b444c
joshiebudd <joshiebudd@gmail.com>
2024-03-15 12:16:02 +0000

    style(apos)

--- components/CourseFormAlpha.js
+++ components/CourseFormAlpha.js
@@ -64,13 +64,6 @@ const CourseFormAlpha = () => {
             </div>
           </div>
         </div>
-        <div className="pt-3 flex justify-center pb-16">
-          <p className="max-w-[500px] text-left italic leading-5 text-gray-700">
-            'by clicking the button above, you are consenting to receive emails
-            from us. you can opt out of this immediately but it is required for
-            delivery.'
-          </p>
-        </div>
       </div>
     </section>
   );
