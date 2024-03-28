65db00faa31b2077a215f7500de8a7f0fe45181f
joshiebudd <joshiebudd@gmail.com>
2024-03-28 13:20:37 +0000

    style(path)

--- components/HeroSection.js
+++ components/HeroSection.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import styles from '../styles/strikethrough.module.css';
+import styles from '../../styles/strikethrough.module.css';
 
 const HeroSection = () => {
   const handleButtonClick = (e) => {
