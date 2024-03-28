20ae50e3feb6da1115dfedff2c469023fb4d1f5e
joshiebudd <joshiebudd@gmail.com>
2024-03-28 18:42:17 +0000

    style(bug)

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,20 +4,20 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}", // Note the addition of the `app` directory.
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    letterSpacing: {
-      tightest: "-.01em",
-    },
-    fontFamily: {
-      manrope: ['manrope', 'sans'],
-      inter: ['inter', 'sans'],
-      baloo: ['"Baloo 2"', 'sans-serif'],
+    extend: {
+      letterSpacing: {
+        tightest: '-.01em',
+      },
+      fontFamily: {
+        manrope: ['manrope', 'sans'],
+        inter: ['inter', 'sans'],
+        baloo: ['"Baloo 2"', 'sans-serif'],
+      },
     },
   },
-},
-plugins: [],
+  plugins: [],
 };
