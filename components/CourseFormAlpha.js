cb697abf49569ee881455e103eeae4dfad6c79b6
joshiebudd <joshiebudd@gmail.com>
2024-03-28 12:16:48 +0000

    style(alpha)

--- components/CourseFormAlpha.js
+++ components/CourseFormAlpha.js
@@ -13,25 +13,25 @@ const CourseFormAlpha = () => {
   return (
     <section className="bg-gray-100 pt-24 sm:pt-32">
       <div className="text-center px-4">
-      <div className=" flex justify-center">
-  <h2 className="md:max-w-[600px] text-2xl md:text-4xl leading-6 text-center">
-    <span className="font-extrabold">[</span>
-    <span className="font-extrabold text-red-700">NEW</span>
-    <span className="font-extrabold">]</span>{" "}
-    <span className="font-bold text-black">
-      Discover the System that Helps ADHDers to Switch into Focus in 2-6 Minutes
-      Whenever They Need to Study
-    </span>
-  </h2>
-</div>
+        <div className="flex justify-center">
+          <h2 className="md:max-w-[600px] text-2xl md:text-4xl leading-6 text-center">
+            <span className="font-extrabold">[</span>
+            <span className="font-extrabold text-red-700">NEW</span>
+            <span className="font-extrabold">]</span>{" "}
+            <span className="font-bold text-black">
+              Discover the System that Helps ADHDers to Switch into Focus in 2-6 Minutes
+              Whenever They Need to Study
+            </span>
+          </h2>
+        </div>
         <div className="timer-widget flex justify-center height-auto pt-6 pb-12">
           <Image
             className="md:w-[50%] w-[80%]"
             loading="eager"
             src="https://raw.githubusercontent.com/joshiebudd/neuronotion/main/public/ipaddemo.png"
             alt="templateipad"
-            width={800} // Set the width of the image (you can adjust as needed)
-            height={566} // Set the height of the image (adjust for the aspect ratio of your image)
+            width={800}
+            height={566}
             priority={1}
           />
         </div>
@@ -81,6 +81,59 @@ const CourseFormAlpha = () => {
                 </button>
               </div>
             </div>
+          </div>
+          <p>
+            PS: When you sign up for the guide, you will also get a link to set up
+            the timer and audio player tool gift promised in the video above in your
+            email. There will also be special tips on how to maximise the tool. So
+            no need to get it from here.
+          </p>
+          <br />
+          <p>If you missed it above, here&apos;s the form again to get the productivity system guide:</p>
+          <div className="sign-form sm:mx-auto sm:w-full sm:max-w-sm">
+            <div className="space-y-2">
+              <div>
+                <div className="mt-2">
+                  <input
+                    id="name"
+                    name="name"
+                    autoComplete="email"
+                    placeholder="Enter Name"
+                    required
+                    className="block pl-2 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                  />
+                </div>
+              </div>
+              <div>
+                <div className="mt-2 mb-4">
+                  <input
+                    id="email"
+                    name="email"
+                    type="email"
+                    placeholder="Enter Email Address"
+                    autoComplete="email"
+                    required
+                    className="block pl-2 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="flex justify-center">
+            <button
+              type="submit"
+              className="flex w-80 justify-center rounded-md bg-sky-800 px-3 py-1.5 mb-4 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              onClick={() => {
+                onGetAccessClicked();
+                track("Course_Checkout");
+              }}
+            >
+              Only $2 to get organised? Yes please!
+            </button>
           </div>
         </div>
       </div>
