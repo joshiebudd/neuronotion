4fb378b5089471562896fc641f4f346b3c2a17a1
joshiebudd <joshiebudd@gmail.com>
2024-03-28 11:43:08 +0000

    style(beta)

--- components/CourseFormBeta.js
+++ components/CourseFormBeta.js
@@ -51,12 +51,52 @@ const CourseFormBeta = () => {
                 track("Course_Checkout");
               }}
             >
-              Yes to a lasting productivity solution!
+              I want a lasting productivity solution!
             </button>
           </div>
         </div>
       </div>
-      <FunnelCopy />
+      <p>
+        PS: When you sign up for the guide, you will also get a link to set up
+        the timer and audio player tool gift promised in the video above in your
+        email. There will also be special tips on how to maximise the tool. So
+        no need to get it from here.
+      </p>
+      <br />
+      <p>If you missed it above, here&apos;s the form again to get the productivity system guide:</p>
+      <div className="sign-form sm:mx-auto sm:w-full sm:max-w-sm">
+        <div class="space-y-2">
+          <div>
+            <div class="mt-2">
+              <input
+                id="name"
+                name="name"
+                autocomplete="email"
+                placeholder="Enter Name"
+                required
+                class="block pl-2 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              ></input>
+            </div>
+          </div>
+          <div>
+            <div class="mt-2 mb-4">
+              <input
+                id="email"
+                name="email"
+                type="email"
+                placeholder="Enter Email Address"
+                autocomplete="email"
+                required
+                class="block pl-2 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
       <div className="flex justify-center mb-8">
         <button
           type="submit"
@@ -70,38 +110,6 @@ const CourseFormBeta = () => {
         </button>
       </div>
     </div>
-  );
-};
-
-const FunnelCopy = () => {
-  return (
-    <section className="article-section" style={{ textAlign: "left" }}>
-      <p>Hey Friend,</p>
-      <br />
-      <p>
-        Having ADHD should not mean you cannot thrive in life, hobbies and
-        relationships.
-      </p>
-      <br />
-      <p>And it definitely should not condemn you to failing in school.</p>
-      <br />
-      <p>
-        As an ADHDer, I have learnt how to flourish in a world that is designed
-        for people with neurotypical brains.
-      </p>
-      <br />
-      <p>
-        PS: When you sign up for the guide, you will also get a link to set up
-        the timer and audio player tool gift promised in the video above in your
-        email. There will also be special tips on how to maximise the tool. So
-        no need to get it from here.
-      </p>
-      <br />
-      <p>
-        If you missed it above, here's the form again to get the productivity
-        system guide:
-      </p>
-    </section>
   );
 };
 
