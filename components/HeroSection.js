0d0be1ac774364c5bafc77eb11277aeb9cc8834d
joshiebudd <joshiebudd@gmail.com>
2024-04-02 15:33:58 +0100

    style(Hero)

--- components/HeroSection.js
+++ components/HeroSection.js
@@ -14,14 +14,15 @@ const HeroSection = () => {
   return (
 <section id="hero" class="bg-gray-100 hero-section pt-24 sm:pt-32">
   <div class="mx-auto max-w-screen-xl px-4 py-8 text-center lg:px-12 lg:py-16">
-    <h1 class="mb-10 font-baloo font-bold leading-none tracking-tight text-black lg:text-5xl text-4xl relative inline-block">
-      A Focus 
-      <span class="relative text-black inline-block">
-        <span class="absolute inset-x-0 top-0 mx-auto text-4xl lg:text-5xl font-baloo font-bold text-blue-500 transform -translate-y-full">System</span>
-        <span class="line-through decoration-wavy text-gray-700 decoration-blue-500">&quot;Hack&quot;</span>
-      </span>
-      to help <br /> your ADHD Brain Study.
-    </h1>
+  <Image
+        style={{ width: "100%" }}
+        loading="eager"
+        src="https://raw.githubusercontent.com/joshiebudd/neuronotion/main/public/heroTitle.png"
+        alt="titleheader"
+        width={640} // Set the width of the image (you can adjust as needed)
+        height={240} // Set the height of the image (adjust for the aspect ratio of your image)
+        priority={1}
+      />
     <p className="mb-10 text-xl font-baloo font-regular text-black sm:px-18 xl:px-24 mt-3">
     Drown the noise in 90 seconds and study for as long as you want. This Notion template helps you focus fast. And with tools that make studying fun and effective, you won&apos;t need a new TikTok hack anytime soon.
     </p>
