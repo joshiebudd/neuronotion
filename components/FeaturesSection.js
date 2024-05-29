82addd87a4859d8a9c6338f44cdacbe837ff70cb
joshiebudd <joshiebudd@gmail.com>
2024-05-29 16:39:58 +0100

    style(reinstate)

--- components/FeaturesSection.js
+++ components/FeaturesSection.js
@@ -1,118 +1,141 @@
-import React from 'react';
-import 'flowbite';
-import 'flowbite-typography';
-import 'tailwindcss/tailwind.css';
-import HeaderSection from '../components/HeaderSection';
+import React from "react";
+import Image from "next/image";
+import CalendarIcon from "./Common/Icons/CalendarIcon";
+import BrainIcon from "./Common/Icons/BrainIcon";
+import TutorialIcon from "./Common/Icons/TutorialIcon";
+import MigrationIcon from "./Common/Icons/MigrationIcon";
+import BrushIcon from "./Common/Icons/BrushIcon";
+import FolderIcon from "./Common/Icons/FolderIcon";
 
-const StudyMusicBlog = () => {
+const FeaturesSection = () => {
+  const handleButtonClick = (e) => {
+    logFeaturesBuyNowEvent(); // Log the FeaturesBuyNowClick event
+  };
+
+  const logFeaturesBuyNowEvent = () => {
+    // Log event to Facebook Pixel for Features Buy Now click
+    fbq("trackCustom", "FeaturesBuyNowClick");
+  };
+
   return (
-    <main className="pt-40 pb-16 lg:pb-24 bg-white dark:bg-gray-900 antialiased">
-      <div className="flex justify-between px-4 mx-auto max-w-screen-xl">
-        <HeaderSection />
-        <article className="mx-auto w-full max-w-2xl format format-sm sm:format-base lg:format-lg format-blue dark:format-invert">
-          <header className="mb-4 lg:mb-6 not-format">
-            <address className="flex items-center mb-6 not-italic">
-              <div className="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white">
-                <img
-                  className="mr-4 w-16 h-16 rounded-full"
-                  src="https://flowbite.com/docs/images/people/profile-picture-2.jpg"
-                  alt="Jese Leos"
-                />
-                <div>
-                  <a rel="author" className="text-xl font-bold text-gray-900 dark:text-white">
-                    Jese Leos
-                  </a>
-                  <p className="text-base text-gray-500 dark:text-gray-400">
-                    Graphic Designer, educator & CEO Flowbite
-                  </p>
-                  <p className="text-base text-gray-500 dark:text-gray-400">
-                    <time pubdate dateTime="2022-02-08" title="February 8th, 2022">
-                      Feb. 8, 2022
-                    </time>
-                  </p>
-                </div>
-              </div>
-            </address>
-            <h1 className="mb-4 text-3xl font-extrabold leading-tight text-gray-900 lg:mb-6 lg:text-4xl dark:text-white">
-              Best practices for successful prototypes
-            </h1>
-          </header>
-          <p className="lead">
-            Flowbite is an open-source library of UI components built with the utility-first
-            classes from Tailwind CSS. It also includes interactive elements such as dropdowns,
-            modals, datepickers.
-          </p>
-          <p>
-            Before going digital, you might benefit from scribbling down some ideas in a sketchbook. This way,
-            you can think things through before committing to an actual design project.
-          </p>
-          <p>
-            But then I found a component library based on Tailwind CSS called Flowbite. It comes with the most
-            commonly used UI components, such as buttons, navigation bars, cards, form elements, and more which
-            are conveniently built with the utility classes from Tailwind CSS.
-          </p>
-          <figure>
-            <img src="https://flowbite.s3.amazonaws.com/typography-plugin/typography-image-1.png" alt="" />
-            <figcaption>Digital art by Anonymous</figcaption>
-          </figure>
-          <h2>Getting started with Flowbite</h2>
-          <p>
-            First of all you need to understand how Flowbite works. This library is not another framework.
-            Rather, it is a set of components based on Tailwind CSS that you can just copy-paste from the
-            documentation.
-          </p>
-          <p>
-            It also includes a JavaScript file that enables interactive components, such as modals, dropdowns,
-            and datepickers which you can optionally include into your project via CDN or NPM.
-          </p>
-          <p>
-            You can check out the quickstart guide to explore the elements by including the CDN files into your
-            project. But if you want to build a project with Flowbite I recommend you to follow the build tools
-            steps so that you can purge and minify the generated CSS.
-          </p>
-          <p>
-            You'll also receive a lot of useful application UI, marketing UI, and e-commerce pages that can help
-            you get started with your projects even faster. You can check out this comparison table to better
-            understand the differences between the open-source and pro version of Flowbite.
-          </p>
-          <h3>Laying the best for successful prototyping</h3>
-          <p>
-            A serif is a small shape or projection that appears at the beginning:
-          </p>
-          <blockquote>
-            <p>
-              Flowbite is just awesome. It contains tons of predesigned components and pages starting from
-              login screen to complex dashboard. Perfect choice for your next SaaS application.
+    <section
+      id="features"
+      className="pt-12 bg-blue-50 features_section mb-0 pb-8">
+      <div className="sm:pb-20 px-4">
+      <div className="pb-8 sm:pb-0 px-8 mx-auto max-w-screen-xl sm:pt-4 lg:px-12">
+        <div className="max-w-screen-md mx-auto text-center mb-1 mt-2">
+          <div className="inline-block px-3 py-1 mb-6 bg-blue-300 rounded-full text-xs text-grey-950 font-baloo font-semibold">
+            Features
+            </div>
+            <div className="text-4xl text-left sm:text-center font-baloo font-bold text-gray-800">
+            Finally...
+            <span className="from-blue-400 to-blue-700 bg-gradient-to-tr bg-clip-text text-transparent">
+              <br />One home for everything </span>🏠
+            </div>
+            <p className="mt-6 text-left sm:text-center max-w-md mx-auto font-baloo text-gray-900 pb-8 text-lg">
+              The all-in-one system your brain has been crying out for. No more juggling 15 tabs, no more distractions.
             </p>
-          </blockquote>
-          <h4>Code example</h4>
-          <p>
-            A serif is a small shape or projection that appears at the beginning or end of a stroke on a letter.
-            Typefaces with serifs are called serif typefaces. Serif fonts are classified as one of the
-            following:
-          </p>
-          <pre>
-            <code className="language-html">
-              &lt;dl class="grid grid-cols-2 gap-8 max-w-screen-md text-gray-900 sm:grid-cols-3 dark:text-white"&gt;
-              &lt;div class="flex flex-col justify-center items-center"&gt;
-              &lt;dt class="mb-2 text-3xl font-extrabold"&gt;73M+&lt;/dt&gt;
-              &lt;dd class="text-lg font-normal text-gray-500 dark:text-gray-400"&gt;developers&lt;/dd&gt;
-              &lt;/div&gt;
-              &lt;div class="flex flex-col justify-center items-center"&gt;
-              &lt;dt class="mb-2 text-3xl font-extrabold"&gt;1B+&lt;/dt&gt;
-              &lt;dd class="text-lg font-normal text-gray-500 dark:text-gray-400"&gt;contributors&lt;/dd&gt;
-              &lt;/div&gt;
-              &lt;div class="flex flex-col justify-center items-center"&gt;
-              &lt;dt class="mb-2 text-3xl font-extrabold"&gt;4M+&lt;/dt&gt;
-              &lt;dd class="text-lg font-normal text-gray-500 dark:text-gray-400"&gt;organizations&lt;/dd&gt;
-              &lt;/div&gt;
-              &lt;/dl&gt;
-            </code>
-          </pre>
-        </article>
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {/* Task Card 1 */}
+            <div className="p-7 max-w-sm mx-auto bg-gray-50 rounded-lg shadow-md shadow-gray-300 flex flex-col items-center">
+              <div className="flex-shrink-0">
+                <CalendarIcon color="#ef4444" />
+              </div>
+              <div className="pt-5 text-lg font-baloo font-semibold text-gray-900">
+                Super Easy Organisation
+              </div>
+              <p className="pt-1 font-baloo text-gray-700 text-center">
+                No more distractions. Set tasks, reminders, deadlines, schedules
+                all in one place.
+              </p>
+            </div>
+
+            {/* Task Card 2 */}
+            <div className="p-7 max-w-sm mx-auto bg-gray-50 rounded-lg shadow-md shadow-gray-300 flex flex-col items-center">
+              <div className="flex-shrink-0">
+                <BrainIcon color="#16a34a" />
+              </div>
+              <div className="pt-5 text-lg font-baloo font-semibold text-gray-900">
+                AI Generation Tools
+              </div>
+              <p className="pt-1 font-baloo text-gray-700 text-center">
+                Transform notes into essay plans, summarise lecture topics, and
+                more with Notion AI!
+              </p>
+            </div>
+
+            {/* Task Card 3 */}
+            <div className="p-7 max-w-sm mx-auto bg-gray-50 rounded-lg shadow-md shadow-gray-300 flex flex-col items-center">
+              <div className="flex-shrink-0">
+                <TutorialIcon color="#0284c7" />
+              </div>
+              <div className="pt-5 text-lg font-baloo font-semibold text-gray-900">
+                Clear and Simple Tutorials
+              </div>
+              <p className="pt-1 font-baloo text-gray-700 text-center">
+                {" "}
+                Notion beginners can ease into this system with full how-to
+                videos on every page.
+              </p>
+            </div>
+
+            {/* Task Card 4 */}
+            <div className="p-7 max-w-sm mx-auto bg-gray-50 rounded-lg shadow-md shadow-gray-300 flex flex-col items-center">
+              <div className="flex-shrink-0">
+                <BrushIcon color="#4f46e5" />
+              </div>
+              <div className="pt-5 text-lg font-baloo font-semibold text-gray-900">
+                Make it Your Own
+              </div>
+              <p className="pt-1 font-baloo text-gray-700 text-center">
+                In Notion, customization is easy. Change colors, icons, and
+                layout however you like!
+              </p>
+            </div>
+
+            {/* Task Card 5 */}
+            <div className="p-7 max-w-sm mx-auto bg-gray-50 rounded-lg shadow-md shadow-gray-300 flex flex-col items-center">
+              <div className="flex-shrink-0">
+                <MigrationIcon color="#c026d3" />
+              </div>
+              <div className="pt-5 text-lg font-baloo font-semibold text-gray-900">
+                Seamless Migration
+              </div>
+              <p className="pt-1 font-baloo text-gray-700 text-center">
+                Moving from another platform is as easy as 1,2, CTRL + V ...
+                Just Copy, Paste, and Go!
+              </p>
+            </div>
+
+            {/* Task Card 6 */}
+            <div className="p-7 max-w-sm mx-auto bg-gray-50 rounded-lg shadow-md shadow-gray-300 flex flex-col items-center">
+              <div className="flex-shrink-0">
+                <FolderIcon color="#e11d48" />
+              </div>
+              <div className="pt-5 text-lg font-baloo font-semibold text-gray-900">
+                Everything in one place
+              </div>
+              <p className="pt-1 font-baloo text-gray-700 text-center">
+                Flashcards, Revision Notes, Essay plans, and Extra Reading - all
+                just one click away.
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
-    </main>
+
+      <div className="flex flex-col sm:flex-row justify-center items-center space-x-0 sm:space-x-4 sm:mt-[-40px]">
+          <a
+            href="#pricing"
+            onClick={handleButtonClick}
+            className="StandardCheckoutButton inline-block bg-[conic-gradient(at_left,_var(--tw-gradient-stops))] from-blue-400 to-blue-700 mb-2 rounded-lg px-5 py-3 text-lg font-baloo font-bold text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-gray-300"
+          >
+            Get for Free.
+          </a>
+        </div>
+    </section>
   );
 };
 
-export default StudyMusicBlog;
+export default FeaturesSection;
