e1fdef90160727d1e0b7db941081d767e9a70aa0
joshiebudd <joshiebudd@gmail.com>
2024-05-29 15:40:40 +0100

    style(tester)

---
+++ pages/becomeatester.js
@@ -0,0 +1,41 @@
+import React, { useEffect } from "react";
+import { track } from "@vercel/analytics";
+import Head from "next/head";
+import FooterSection from "../components/FooterSection";
+
+
+
+const BecomeATester = () => {
+  <Head>
+    <meta
+      name="Join the testers"
+      content="Join the waiting list to become a product tester"
+    />
+  </Head>;
+  useEffect(() => {
+    const startTime = Date.now();
+
+    const handleUnload = () => {
+      const endTime = Date.now();
+      const timeSpent = endTime - startTime;
+
+      // Send timeSpent to analytics service
+      console.log("Time spent on page:", timeSpent);
+    };
+
+    window.addEventListener("beforeunload", handleUnload);
+
+    return () => {
+      window.removeEventListener("beforeunload", handleUnload);
+    };
+   
+  }, []);
+
+  return (
+    <>
+      <FooterSection />
+    </>
+  );
+};
+
+export default BecomeATester;
