3ae8b91649dab9a5f8e098e353461df0205153f4
joshiebudd <joshiebudd@gmail.com>
2024-06-04 16:02:13 +0100

    style(mobfix)

--- components/WaitingListHeader.js
+++ components/WaitingListHeader.js
@@ -1,20 +1,26 @@
-import React, { useEffect, useRef, useCallback, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { SpeedInsights } from "@vercel/speed-insights/react";
 import { track } from "@vercel/analytics";
 import Image from "next/image";
 import Script from "next/script";
-import * as _ from "lodash";
+import Burger from "./Common/Burger";
+import _ from "lodash";
+
 import styles from "./HeaderSection.style.module.scss";
 
 const WaitingListHeaderSection = () => {
   const titleRef = useRef(null);
   const menuRef = useRef(null);
-  const [isMobile, setIsMobile] = useState(false);
+  const [menuActive, setMenuActive] = useState(false);
 
-  const handleResize = useCallback(() => {
-    const newIsMobile = window.innerWidth < 640;
+  let isMobile = false;
+
+  const handleResize = () => {
+    const newIsMobile = innerWidth < 640;
+
     if (newIsMobile !== isMobile) {
-      setIsMobile(newIsMobile);
+      isMobile = newIsMobile;
+
       if (newIsMobile) {
         menuRef.current.classList.add(styles.menu_mobile);
       } else {
@@ -22,26 +28,48 @@ const WaitingListHeaderSection = () => {
         menuRef.current.classList.remove(styles.mobile_menu_open);
         menuRef.current.classList.remove(styles.mobile_menu_close);
         titleRef.current.style.opacity = 1;
+        setMenuActive(false);
       }
     }
-  }, [isMobile]);
+  };
 
   const throttledHandleResize = _.throttle(handleResize, 300);
 
   useEffect(() => {
-    if (typeof window !== "undefined") {
-      handleResize();
-      window.addEventListener("resize", throttledHandleResize);
-    }
+    handleResize();
+    window.addEventListener("resize", throttledHandleResize);
     return () => {
-      if (typeof window !== "undefined") {
-        window.removeEventListener("resize", throttledHandleResize);
-      }
+      window.removeEventListener("resize", throttledHandleResize);
     };
-  }, [handleResize, throttledHandleResize]);
+  }, []);
 
-  const handleLogoClick = () => {
-    window.location.href = "https://www.neuro-notion.com";
+  const handleButtonClick = (e) => {
+    logCustomPixelEvent("HeaderBuyNowClick");
+  };
+
+  const logCustomPixelEvent = (eventName) => {
+    fbq("trackCustom", "HeaderBuyNowClick");
+  };
+
+  const showMenu = () => {
+    titleRef.current.style.opacity = "0";
+    menuRef.current.classList.add(styles.mobile_menu_open);
+    menuRef.current.classList.remove(styles.mobile_menu_close);
+  };
+
+  const hideMenu = () => {
+    titleRef.current.style.opacity = "1";
+    menuRef.current.classList.add(styles.mobile_menu_close);
+    menuRef.current.classList.remove(styles.mobile_menu_open);
+  };
+
+  const handleBurgerClick = () => {
+    setMenuActive(!menuActive);
+    if (!menuActive) {
+      showMenu();
+    } else {
+      hideMenu();
+    }
   };
 
   return (
@@ -51,27 +79,28 @@ const WaitingListHeaderSection = () => {
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '230622039592089');
-            fbq('track', 'PageView');
-          `,
+                        !function(f,b,e,v,n,t,s)
+                        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                        n.queue=[];t=b.createElement(e);t.async=!0;
+                        t.src=v;s=b.getElementsByTagName(e)[0];
+                        s.parentNode.insertBefore(t,s)}(window, document,'script',
+                        'https://connect.facebook.net/en_US/fbevents.js');
+                        fbq('init', '230622039592089');
+                        fbq('track', 'PageView');
+                    `,
         }}
       />
+
       <SpeedInsights />
+
       <div className="full-header bg-white bg-opacity-30 backdrop-blur-lg fixed top-0 right-0 left-0">
         <nav className="px-8 py-5 lg:px-6">
-          <div className="mx-auto flex max-w-screen-xl flex-wrap items-center justify-center">
+          <div className="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between">
             <div
-              className="flex items-center grow transition duration-300 cursor-pointer justify-center"
+              className="flex items-center grow transition duration-300"
               ref={titleRef}
-              onClick={handleLogoClick}
             >
               <Image
                 width={56}
@@ -81,10 +110,30 @@ const WaitingListHeaderSection = () => {
                 src="https://raw.githubusercontent.com/joshiebudd/notionwidgets/main/logo.webp"
                 alt="Neuro Notion Logo"
               />
-              <div className="text-center">
-                <span className="self-center whitespace-nowrap text-2xl font-baloo font-bold text-black">
+              <div className="w-full grow text-center sm:text-left">
+                <span className="self-center whitespace-nowrap text-2xl sm:text-2xl font-baloo font-bold text-black">
                   Neuro Notion
                 </span>
+              </div>
+            </div>
+            <div
+              className="hidden lg:flex items-center grow overflow-hidden mx-2 min-w-[200px]"
+              ref={menuRef}
+            />
+            <div>
+              <a
+                href="https://buy.stripe.com/00gdT123dbNg9TG28r"
+                onClick={handleButtonClick}
+                className="inline-block rounded-lg bg-[conic-gradient(at_left,_var(--tw-gradient-stops))] from-blue-400 to-blue-700 px-4 py-4 text-lg font-bold text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-gray-300 lg:px-5 lg:py-3 hidden sm:block"
+              >
+                Secure your spot.
+              </a>
+
+              <div className="block sm:hidden">
+                <Burger
+                  handleToggle={handleBurgerClick}
+                  isActive={menuActive}
+                />
               </div>
             </div>
           </div>
