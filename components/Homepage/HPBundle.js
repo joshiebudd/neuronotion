91464e93dab7d9133437135e8eb84c0f10e29015
joshiebudd <joshiebudd@gmail.com>
2024-08-02 10:14:52 +0100

    mobile

--- components/Homepage/HPBundle.js
+++ components/Homepage/HPBundle.js
@@ -3,8 +3,8 @@ import React from 'react';
 const HPBundleSection = () => {
   return (
     <section className="flex justify-center items-center py-8 bg-white">
-      <div className="relative shadow-xl px-8 py-8 bg-sec bg-opacity-15 rounded-3xl max-w-5xl mx-auto text-left">
-        <div className="relative flex justify-center items-center mb-8">
+      <div className="relative shadow-xl md:px-8 py-8 bg-sec bg-opacity-15 rounded-3xl max-w-5xl mx-4 md:mx-auto text-left">
+        <div className="relative flex justify-center items-center mb-2 md:mb-6">
           <a href="https://neuronotionn.gumroad.com/l/adhdtaskmanager?_gl=1*1x78gja*_ga*MzkwMjUxNjgwLjE3MTMyMDY2OTc.*_ga_6LJN6D94N6*MTcyMjUyNTU5Ny4xNjAuMS4xNzIyNTI3MTMwLjAuMC4w" className="relative -mr-80 ml-4 z-0 transform -rotate-6 transition-transform duration-500 hover:scale-105">
             <img
               src="https://NeuroNotionPullZonw.b-cdn.net/Compressed%20Shadow%20Demo%20WEBPs/TaskManagerShadowedDemo.webp"
@@ -33,10 +33,11 @@ const HPBundleSection = () => {
             />
           </a>
         </div>
-        <div className="text-center">
-          <h2 className="text-3xl mt-4 text-prim font-prim font-bold">Free ADHD Notion Starter Bundle</h2>
-          <p className="text-slate-700 font-prim text-md mb-4"> Get free access to 4x ADHD Notion Templates to start taking back control. </p>
+        <div className="mx-3 md:mx-0 text-center">
+          <h2 className="text-3xl md:mt-4 text-prim font-prim font-bold">Free ADHD Notion Starter Bundle</h2>
+          <p className="text-slate-700 font-prim text-md mx-3 md:mx-0 mt-2 md:mx-0 mb-4"> Get free access to 4x ADHD Notion Templates to start taking back control. </p>
           <a href="https://www.neuro-notion.com/templates" className="btn btn-primary">
+
           <button className="bg-prim text-sec py-3 px-10 font-prim font-bold rounded-xl text-xl shadow-gray-600 shadow-md hover:shadow-gray-600 hover:shadow-sm hover:bg-sec hover:text-prim">
             Get For Free
           </button>
--- components/Homepage/HPUB.js
+++ components/Homepage/HPUB.js
@@ -3,31 +3,32 @@ import React from 'react';
 const HPUBSection = () => {
   return (
     <section className="flex justify-center items-center py-8 pb-20 bg-white">
-      <div className="shadow-xl px-16 py-8 bg-accent1 bg-opacity-15 rounded-3xl max-w-5xl mx-auto text-left">
-        <div className="relative z-1 transform rotate-0 transition-transform duration-500 hover:scale-105">
-        <a href="https://neuronotionn.gumroad.com/l/ultimateadhdbrain?_gl=1*tx75fr*_ga*MzkwMjUxNjgwLjE3MTMyMDY2OTc.*_ga_6LJN6D94N6*MTcyMjUzNDk3Ny4xNjEuMC4xNzIyNTM0OTc3LjAuMC4w" target="_blank">
-        <img
-         src="https://NeuroNotionPullZonw.b-cdn.net/Compressed%20Shadow%20Demo%20WEBPs/UBDarkModeDemoRoundedShadowFinal.webp"
-         alt="ADHD Notion Starter Bundle"
-         className="px-8 py-4 w-full max-w-5xl"
-        />
-        </a>
+      <div className="relative shadow-xl md:px-16 py-8 bg-accent1 bg-opacity-15 rounded-3xl max-w-5xl mx-4 md:mx-auto text-left">
+        <div className="relative z-1 transform rotate-0 transition-transform duration-500 hover:scale-105 mb-6 md:mb-0">
+          <a href="https://neuronotionn.gumroad.com/l/ultimateadhdbrain?_gl=1*tx75fr*_ga*MzkwMjUxNjgwLjE3MTMyMDY2OTc.*_ga_6LJN6D94N6*MTcyMjUzNDk3Ny4xNjEuMC4xNzIyNTM0OTc3LjAuMC4w" target="_blank">
+            <img
+              src="https://NeuroNotionPullZonw.b-cdn.net/Compressed%20Shadow%20Demo%20WEBPs/UBDarkModeDemoRoundedShadowFinal.webp"
+              alt="ADHD Notion Starter Bundle"
+              className="w-full max-w-5xl rounded-lg"
+            />
+          </a>
         </div>
-        <div className="flex justify-between items-center">
-          <div>
+        <div className="mx-4 md:mx-0 md:flex md:justify-between md:items-center">
+          <div className="text-center md:text-left mb-4 md:mb-0">
             <h2 className="text-3xl mt-4 text-prim font-prim font-bold">
-              Your new best friend... <br /> <span className="text-accent2">Ultimate ADHD Brain 2.0</span>
+              Your new best friend... <br className="hidden md:block" /> <span className="text-accent2">Ultimate ADHD Brain 2.0</span>
             </h2>
             <p className="text-slate-700 font-prim text-md mb-4">
-              Your all-in-one ADHD Life Management Hub. <br /> Let your executive function relax at last...
+              Your all-in-one ADHD Life Management Hub. <span className="hidden md:inline"><br /></span> Let your executive function relax at last...
             </p>
           </div>
-          <a href="https://neuronotionn.gumroad.com/l/ultimateadhdbrain?_gl=1*139oplk*_ga*MzkwMjUxNjgwLjE3MTMyMDY2OTc.*_ga_6LJN6D94N6*MTcyMjUyNTU5Ny4xNjAuMS4xNzIyNTI1NzE1LjAuMC4w" className="inline-block">
-           <button className="bg-accent2 text-gray-50 py-3 px-6 font-prim font-bold rounded-xl text-xl shadow-gray-600 shadow-md hover:shadow-gray-600 hover:shadow-sm hover:bg-sec hover:text-prim">
-            <span className="text-gray-50">Learn More</span>
-            </button>
-          </a>
-
+          <div className="text-center md:text-left">
+            <a href="https://neuronotionn.gumroad.com/l/ultimateadhdbrain?_gl=1*139oplk*_ga*MzkwMjUxNjgwLjE3MTMyMDY2OTc.*_ga_6LJN6D94N6*MTcyMjUyNTU5Ny4xNjAuMS4xNzIyNTI1NzE1LjAuMC4w" className="inline-block">
+              <button className="bg-accent2 text-gray-50 py-3 px-6 font-prim font-bold rounded-xl text-xl shadow-gray-600 shadow-md hover:shadow-gray-600 hover:shadow-sm hover:bg-sec hover:text-prim">
+                <span className="text-gray-50">Learn More</span>
+              </button>
+            </a>
+          </div>
         </div>
       </div>
     </section>
