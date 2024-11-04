f697a0bf7510d051fb4977d9cb9ecda2d620aa33
joshiebudd <joshiebudd@gmail.com>
2024-11-04 13:39:11 +0000

    AI Assistant

--- pages/_app.js
+++ pages/_app.js
@@ -37,6 +37,23 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <PostHogProvider client={posthog}>
+      <Script
+        id="tawk-script"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+            (function(){
+              var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+              s1.async=true;
+              s1.src='https://embed.tawk.to/6728aa264304e3196adcd092/1ibrdh9kq';
+              s1.charset='UTF-8';
+              s1.setAttribute('crossorigin','*');
+              s0.parentNode.insertBefore(s1,s0);
+            })();
+          `
+        }}
+      />
       <>
         <Script id="vercel-speed-insights" src="/_vercel/insights/script.js" />
 
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,14 @@
 import '@/styles/globals.css'
+import { PostHogProvider } from 'posthog-js'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <PostHogProvider client={posthog}>
+      <>
+        <Script id="vercel-speed-insights" src="/_vercel/insights/script.js" />
+        <Component {...pageProps} />
+      </>
+    </PostHogProvider>
+  )
 }
