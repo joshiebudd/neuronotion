5a208f20b3e748df266248925265d408dfbcb61c
joshiebudd <1.40086428e+08+joshiebudd@users.noreply.github.com>
2026-05-21 08:33:40 +0000

    Remove British Business Bank logo from all logo carousels

--- pages/forclinics.js
+++ pages/forclinics.js
@@ -55,7 +55,6 @@ const SupportedByCarousel = () => {
     { id: 5, src: 'https://NeuroNotionPullZonw.b-cdn.net/SupportedByLogos/5.webp', alt: 'Supported by logo 5' },
     { id: 8, src: 'https://NeuroNotionPullZonw.b-cdn.net/SupportedByLogos/8.webp', alt: 'Supported by logo 8' },
     { id: 3, src: 'https://NeuroNotionPullZonw.b-cdn.net/SupportedByLogos/3.webp', alt: 'Supported by logo 3' },
-    { id: 9, src: 'https://NeuroNotionPullZonw.b-cdn.net/SupportedByLogos/9.webp', alt: 'Supported by logo 9' },
     { id: 12, src: 'https://NeuroNotionPullZonw.b-cdn.net/11LabsLogowebp.webp', alt: '11Labs' },
     { id: 13, src: 'https://NeuroNotionPullZonw.b-cdn.net/SupportedByLogos/12.webp', alt: 'Supported by logo 12' },
   ];
